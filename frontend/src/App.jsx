fecaa9ee64b9bf0c805a61a868e1bf0e1dff4e98
KTH0920 <taehyun9482@gmail.com>
2025-11-24 18:44:53 +0900

    feat: frontned layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
-import Landing from './pages/Landing';
-import SearchResults from './pages/SearchResults';
-import Favorites from './pages/Favorites';
-import HotelDetail from './pages/HotelDetail';
-import Booking from './pages/Booking';
-import BookingConfirmation from './pages/BookingConfirmation';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
-import Account from './pages/Account';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AdminAuthProvider } from "./context/AdminAuthContext";
+import adminRoutes from "./router/adminRoutes";
+import "./styles/index.scss";
 
 function App() {
   return (
-    <div className="app-container">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/hotel/:id" element={<HotelDetail />} />
-        <Route path="/hotel/:id/booking/:roomId" element={<Booking />} />
-        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <AdminAuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin/login" replace />} />
+          {adminRoutes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element}>
+              {route.children?.map((child, childIndex) => (
+                <Route
+                  key={childIndex}
+                  index={child.index}
+                  path={child.path}
+                  element={child.element}
+                />
+              ))}
+            </Route>
+          ))}
+        </Routes>
+      </AdminAuthProvider>
+    </BrowserRouter>
   );
 }
 
